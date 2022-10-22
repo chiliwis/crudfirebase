e0dc30e8bc7dfca2761fa946e55efb0de25ee07e
Cristina SG <cristinasanchezg@gmail.com>
2022-10-22 13:17:21 -0500

    datos2

--- src/pages/Tabla.jsx
+++ src/pages/Tabla.jsx
@@ -1,16 +1,21 @@
 
-import { collection,  getDocs } from "firebase/firestore";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
 import { db } from '../firebase';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 
 
 function Tabla() {
-    const [datos, setDatos] = useState ([]);
+    const [datos, setDatos] = useState([]);
     const getData = async () => {
-        const snapshot = await getDocs(collection(db, "empleados"));
-        console.log(snapshot.docs.map(doc => doc.data()));
-        setDatos(snapshot.docs.map(doc => doc.data()));
+        // const snapshot = await onSnapshot(collection(db, "empleados"));
+        // console.log(snapshot.docs.map(doc => doc.data()));
+        // setDatos(snapshot.docs.map(doc => doc.data()));
+        onSnapshot(collection(db, "empleados"), (querySnapshot) => {
+            setDatos(querySnapshot.docs.map(doc => {
+                return { ...doc.data() }
+            }));
+        });
     };
 
     useEffect(() => {
@@ -18,37 +23,37 @@ function Tabla() {
     }, []);
 
 
-    return ( 
-<>
-{/* <code>{JSON.stringify(datos)}</code> */}
-       <table className="table table-bordered">
-  <thead classNAme="table-dark">
-    <tr>
-      <th>#</th>
-      <th>Nombre</th>
-      <th>Puesto</th>
-      <th>Fecha contratacion</th>
-    </tr>
-  </thead>
-  <tbody>
-  {
-    datos.map((empleado, index) => {
-        return(
-            <tr key={index}>
-            <th>{index + 1}</th>
-            <td>{empleado.nombre}</td>
-            <td>{empleado.puesto}</td>
-            <td>{empleado.fecha_contratacion}</td>
-          </tr>
-        )
-    })
-  }
+    return (
+        <>
+            {/* <code>{JSON.stringify(datos)}</code> */}
+            <table className="table table-bordered">
+                <thead classNAme="table-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Puesto</th>
+                        <th>Fecha contratacion</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        datos.map((empleado, index) => {
+                            return (
+                                <tr key={index}>
+                                    <th>{index + 1}</th>
+                                    <td>{empleado.nombre}</td>
+                                    <td>{empleado.puesto}</td>
+                                    <td>{empleado.fecha_contratacion}</td>
+                                </tr>
+                            )
+                        })
+                    }
 
-  </tbody>
-</table>
+                </tbody>
+            </table>
 
 
-</>
+        </>
 
     );
 }
