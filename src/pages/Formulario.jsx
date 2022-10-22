0e0cfae3a02a3e8c3664406343706f76ce544c97
Cristina SG <cristinasanchezg@gmail.com>
2022-10-22 13:55:25 -0500

    Update Formulario.jsx

--- src/pages/Formulario.jsx
+++ src/pages/Formulario.jsx
@@ -1,22 +1,47 @@
-function Formulario() {
-    return (
-        <form>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" />
-  </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
+import { useState } from 'react';
+import { db } from '../firebase';
+import { addDoc, collection, doc } from "firebase/forestore";
 
+function Formulario() {
+    const [formulario, setFormulario] = useState({
+        nombre: '',
+        puesto: '',
+        fecha_contratacion: '',
+    });
+
+    const handleInputChange = (event) => {
+        setFormulario({
+            ...formulario,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const guardarFormulario = async (event) => {
+        event.preventDefault();
+        console.log(formulario);
+        await addDoc(collection(db,"empleados"), formulario);
+    };
+
+    return (
+        <>
+            <code>{JSON.stringify(formulario)}</code>
+            <form onSubmit={guardarFormulario}>
+                <div className="mb-3">
+                    <label className="form-label">Nombre</label>
+                    <input type="text" className="form-control" name='nombre' onChange={handleInputChange} />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Puesto</label>
+                    <input type="text" className="form-control" name='puesto' onChange={handleInputChange} />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Fecha contratacion</label>
+                    <input type="date" className="form-control" name='fecha_contratacion' onChange={handleInputChange} />
+                </div>
+
+                <button type="submit" className="btn btn-primary">Guardar</button>
+            </form>
+        </>
     );
 }
 
